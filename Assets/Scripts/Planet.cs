09c2eb8295b5d823869a24fd4cfb1fc1a739e398
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-11-05 20:35:05 -0500

    Add title screen

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    private const float RotateSpeed = 0.5f;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(0f, RotateSpeed, 0f, Space.Self);
+        // transform.RotateAround(transform.position, Camera.main.transform.up, RotateSpeed);
+    }
+}
